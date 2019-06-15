c1de8de1d36320bc35e200c0c3095d0c7b653f05
Zharif.Hadi @ 123RF <zharif.hadi@123rf.com>
2019-06-15 14:53:03 +0800

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by M. Zharif Hadi M. Khairuddin on 15/06/2019.
+//  Copyright © 2019 M. Zharif Hadi M. Khairuddin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
