582db53885cd3abcf2c5d5d6fa10099e5794a314
Zharif.Hadi @ 123RF <zharif.hadi@123rf.com>
2019-06-16 13:15:12 +0800

    first commit

---
+++ CoreDataDemo/Core Data/CoreDataManager.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataManager.swift
+//  CoreDataDemo
+//
+//  Created by M. Zharif Hadi M. Khairuddin on 15/06/2019.
+//  Copyright © 2019 M. Zharif Hadi M. Khairuddin. All rights reserved.
+//
+
+import Foundation
